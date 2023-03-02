11709c90812055c54b0f502c50e116cfa44594f4
prajwalgunjal <prajwalgunjal8@gmail.com>
2023-03-02 23:25:55 +0530

    Added: UC4

--- Start.java
+++ Start.java
@@ -1,31 +1,34 @@
 package com.bridgelabz;
 public class Start {
-    public static void main(String[] args){
+    private static final int IS_FULL_TIME = 1;
+    private static final int IS_PART_TIME = 2;
+    private static final int EMP_WAGE_PER_HOUR = 20;
 
-        System.out.println(" Welcome to employee wage");
+    public static void main(String[] args) {
 
-        int IS_FULL_TIME = 1;
-        int IS_PART_TIME = 2;
-        int EMP_WAGE_PER_HOUR = 20;
+        System.out.println(" Using switch case ");
 
         int empHrs, empWage;
-        double empCheck = Math.floor(Math.random() * 10) % 3;
-        if(empCheck == IS_FULL_TIME) {
-            System.out.println("Employee is Doing FUll Time.");
-            empHrs = 8;
-        }
+        int empType = (int) Math.floor(Math.random() * 10) % 3;
+        System.out.println("Welcome to Employee Wage Computation");
 
-        else if (empCheck == IS_PART_TIME) {
-            System.out.println("Employee is Doing Part Time.");
-            empHrs = 4;
-        }
-        else {
-            System.out.println("Employee is Absent.");
-            empHrs = 0;
+        switch (empType) {
+            case IS_FULL_TIME:
+                System.out.println("Employee is Doing Full Time:");
+                empHrs = 8;
+                break;
+            case IS_PART_TIME:
+                System.out.println("Employee is Doing Part Time:");
+                empHrs = 4;
+                break;
+            default:
+                System.out.println("Employee is Absent:");
+                empHrs = 0;
+                break;
         }
 
         empWage = empHrs * EMP_WAGE_PER_HOUR;
-        System.out.println("Emp Hours : "+empHrs);
-        System.out.println("Emp Wage : "+empWage);
+        System.out.println("Emp Hours : " + empHrs);
+        System.out.println("Emp Wage : " + empWage);
     }
 }
